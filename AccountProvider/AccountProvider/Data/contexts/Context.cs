7ea2da905108c624cca6a8f9b74a81ed380999f6
HugoEddesten <hugo.eddesten@yh.nakademin.se>
2024-05-10 19:01:18 +0200

    replacement

--- AccountProvider/AccountProvider/Data/contexts/Context.cs
+++
@@ -1,9 +0,0 @@
-﻿using AccountProvider.Data.Entities;
-using Microsoft.EntityFrameworkCore;
-
-namespace AccountProvider.Data.contexts;
-
-public class Context(DbContextOptions<Context> options) : DbContext(options)
-{
-    public DbSet<AccountEntity> Accounts { get; set; }
-}
